f75a6631f6002877d7cda910bbf4460e9c6a1bb8
Ryhor <ryhor.dubrovin@pragmaticplay.com>
2023-08-14 00:01:11 +0400

    upd main

--- src/main/java/org/runner/Main.java
+++ src/main/java/org/runner/Main.java
@@ -1,7 +1,5 @@
 package org.runner;
 
-import org.converter.ClassToJsonConverter;
-
 public class Main {
     public static void main(String[] args) {
         ConverterRunner runner = new ConverterRunner();
