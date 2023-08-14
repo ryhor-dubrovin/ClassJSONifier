8912cc080d346d1567a0d7e6ce467320c7640529
Ryhor <ryhor.dubrovin@pragmaticplay.com>
2023-08-14 09:37:34 +0400

    clear

--- src/main/java/org/runner/ConverterRunner.java
+++ src/main/java/org/runner/ConverterRunner.java
@@ -40,27 +40,4 @@ public class ConverterRunner {
             System.out.println(DIVIDER);
         }
     }
-
-    public void run() {
-
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter class name from [" + CLASSES_PACKAGE_NAME + "*] package to convert: ");
-            String className = scanner.next();
-            if (className.equals("Exit")) break;
-            System.out.println(DIVIDER);
-            if (Character.isLowerCase(className.charAt(0))) {
-                System.out.println("Please capitalize the class name!");
-                System.out.println(DIVIDER);
-                continue;
-            }
-            try {
-                Class<?> clazz = Class.forName(CLASSES_PACKAGE_NAME + className);
-                System.out.println(converter.convertClassToJson(clazz));
-            } catch (ClassNotFoundException e) {
-                System.out.println("No such class in the package =( ");
-            }
-            System.out.println(DIVIDER);
-        }
-    }
 }
